8d76b9ebdde094ab5562a711c5fba6e73a80bd9f
cardos0s <jucsantana05@icloud.com>
2025-05-07 15:20:12 -0300

    feat: adiciona layout base dos cards de criptomoeda e ajustes visuais no Assets

--- Components/Assets.xaml.cs
+++ Components/Assets.xaml.cs
@@ -11,5 +11,21 @@ public partial class Assets : ContentView
     public Assets()
     {
         InitializeComponent();
+
+        ScrollViewHorizontal.HandlerChanged += (s, e) =>
+        {
+
+#if ANDROID
+    var nativeScroll = ScrollViewHorizontal.Handler.PlatformView as Android.Widget.ScrollView;
+    nativeScroll.VerticalScrollBarEnabled = false;
+    nativeScroll.HorizontalScrollBarEnabled = false;
+#endif
+
+#if IOS
+            var nativeScroll = ScrollViewHorizontal.Handler.PlatformView as UIKit.UIScrollView;
+            nativeScroll.ShowsVerticalScrollIndicator = false;
+            nativeScroll.ShowsHorizontalScrollIndicator = false;
+#endif
+        };
     }
 }
